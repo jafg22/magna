c5c8e68f93fa9b4e94772757817f77288617bdb8
egonzalezm24 <emilio.gm00@gmail.com>
2016-04-07 15:13:52 -0600

    Avance en pruebas iniciales chat
    Para arrancar el servidor de WebSocket correr en consola:
     
    # php -q <ruta de document root apache>/magna/u/code/wsserver/server.php

--- u/index.php
+++ u/index.php
@@ -1,19 +1,42 @@
 <?php
+    //BD CONEXION
+    include_once("../rest/clsConexion.inc");
+    $cnx = new mysqlConn("root", "magna", "localhost", "magna");
+    if ($cnx->conectar()){ echo "Conectado a BD"; } else { echo "No conectado a BD"; }
+    //BD CONEXION
+
+    $_SERVER['DOCUMENT_ROOT']."magna/u/code/wsserver/server.php "
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8" http-equiv="content-type">
-    <link href="styles/default.css" type="text/css" rel="stylesheet">
-    <!-- <link href="third-party/bootstrap/bootstrap-3.3.5-dist/css/bootstrap.css" type="text/css" rel="stylesheet"> -->
-    <!-- <link href="third-party/fontAwesome/font-awesome-4.4.0/css/font-awesome.css" type="text/css" rel="stylesheet"> -->
+
+    <!-- ESTILOS CSS -->
+    <link href="styles/default-global.css" type="text/css" rel="stylesheet">
+    <link href="third-party/bootstrap/bootstrap-3.3.5-dist/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="third-party/fontAwesome/font-awesome-4.4.0/css/font-awesome.css" type="text/css" rel="stylesheet">
+
+    <!-- SCRIPTS -->
+    <script src="third-party/jQuery/jquery.min.js"></script>
+    <script src="scripts/default-index.js"></script>
     <title></title>
 </head>
 <body>
-
+    <header></header>
+    <main>
+        <aside>
+            <!-- CHAT -->
+            <div class="chat_wrapper">
+                <div class="message_box" id="message_box"></div>
+                <div class="panel">
+                    <input type="text" name="name" id="name" placeholder="Your Name" maxlength="10" style="width:20%"  />
+                    <input type="text" name="message" id="message" placeholder="Message" maxlength="80" style="width:60%" />
+                    <button id="send-btn">Send</button>
+                </div>
+        </aside>
+    </main>
+    <footer></footer>
 </body>
-
-   <!-- <script src="third-party/jQuery/jquery.min.js"></script> -->
-    <script src="scripts/default.js"></script>
 </html>
