e2e233c1a1664cc9c22ca9c131cae5624e2c09cf
jafg22 <jlfloresg@est.utn.ac.cr>
2016-04-18 20:48:21 -0600

    Corregido

--- rest/codigos/buscaDupli.php
+++ rest/codigos/buscaDupli.php
@@ -3,21 +3,21 @@
  	$conexPDO->conectar();
 
  	if ($_GET['tipo'] == "email") {
- 		$conex->Sqlquery = "select usuario from usuarios where correoU = '".$_GET['valor']."'";
- 		$datos =  $conex->ExecuteSelect();
+ 		$conexPDO->Sqlquery = "select correoU from usuarios where correoU = '".$_GET['valor']."'";
+ 		$datos =  $conexPDO->ExecuteSelect();
  		if (count($datos) > 0) {
- 			deliver_response(200, "OK",array("Existe")); 			
- 		}else {
- 			deliver_response(200, "OK",array("No Existe"));
+ 			deliver_response(200, "OK",array("disponible")); 			
+ 		}else{
+ 			deliver_response(200, "OK",array("no disponible"));
  		}
 
- 	}else{
- 		$conex->Sqlquery = "select correoU from usuarios where usuario = '".$_GET['valor']."'";
- 		$datos =  $conex->ExecuteSelect();
+ 	}else if($_GET['tipo'] == "usu"){
+ 		$conexPDO->Sqlquery = "select usuario from usuarios where usuario = '".$_GET['valor']."'";
+ 		$datos =  $conexPDO->ExecuteSelect();
  		if (count($datos) > 0) {
- 			deliver_response(200, "OK",array("Existe")); 			
+ 			deliver_response(200, "OK",array("disponible")); 			
  		}else {
-			deliver_response(200, "OK",array("No Existe"));
+			deliver_response(200, "OK",array("no disponible"));
  		}
  			
  	}
