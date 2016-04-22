53664cd2f8ab6b4f8032ef2273c6f280f52fd315
egonzalezm24 <emilio.gm00@gmail.com>
2016-04-21 20:28:31 -0600

    arreglo minusculas

--- sql/vistasParaNoticias.sql
+++ sql/vistasParaNoticias.sql
@@ -1,22 +1,22 @@
 CREATE VIEW vst_home_acercade
 AS
-Select idNoticia,tituloN,fechaN,usuarioN,cuerpoN,image,muronoticias.idSeccion,seccionesn.nomSeccion  from muronoticias 
-INNER JOIN seccionesn on seccionesn.idSeccion = muronoticias.idSeccion 
-WHERE muronoticias.idSeccion = 002
-ORDER BY idNoticia DESC LIMIT 3 
+Select idNoticia,tituloN,fechaN,usuarioN,cuerpoN,image,muroNoticias.idSeccion,seccionesN.nomSeccion  from muroNoticias
+INNER JOIN seccionesN on seccionesN.idSeccion = muroNoticias.idSeccion
+WHERE muroNoticias.idSeccion = 002
+ORDER BY idNoticia DESC LIMIT 3;
 
 CREATE VIEW vst_home_cultura
 AS
-Select idNoticia,tituloN,fechaN,usuarioN,cuerpoN,image,muronoticias.idSeccion,seccionesn.nomSeccion  from muronoticias 
-INNER JOIN seccionesn on seccionesn.idSeccion = muronoticias.idSeccion 
-WHERE muronoticias.idSeccion = 001
-ORDER BY idNoticia DESC LIMIT 3 
+Select idNoticia,tituloN,fechaN,usuarioN,cuerpoN,image,muroNoticias.idSeccion,seccionesN.nomSeccion  from muroNoticias
+INNER JOIN seccionesN on seccionesN.idSeccion = muroNoticias.idSeccion
+WHERE muroNoticias.idSeccion = 001
+ORDER BY idNoticia DESC LIMIT 3;
 
 CREATE VIEW vst_noticia_seccion
 AS
-Select idNoticia,tituloN,fechaN,usuarioN,cuerpoN,muronoticias.idSeccion,image, seccionesn.nomSeccion FROM muronoticias
-INNER JOIN seccionesn on seccionesn.idSeccion = muronoticias.idSeccion 
+Select idNoticia,tituloN,fechaN,usuarioN,cuerpoN,muroNoticias.idSeccion,image, seccionesN.nomSeccion FROM muroNoticias
+INNER JOIN seccionesN on seccionesN.idSeccion = muroNoticias.idSeccion;
 
 CREATE VIEW vst_noticia_unica
 AS
-SELECT idNoticia,tituloN,fechaN,usuarioN,image,cuerpoN FROM muronoticias 
+SELECT idNoticia,tituloN,fechaN,usuarioN,image,cuerpoN FROM muroNoticias;
